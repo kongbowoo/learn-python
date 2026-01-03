a085ccf0bfc66964a276f7f636a928099dfa33ab
kongbowoo <kongbowoo@126.com>
2026-01-03 16:59:43 +0800

    first file.

---
+++ operators.py
@@ -0,0 +1,10 @@
+# Arithmetic Operators
+a = 21
+b = 5
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b}') # 标准除法
+print(f'{a} // {b} = {a // b}')  # 整除，向下取整到最近的整数
+print(f'{a} % {b} = {a % b}')  # 取模，返回余数
+print(f'{a} ** {b} = {a ** b}')  # 幂运算，a 的 b 次方
